5ed0a558fb04278665bac20bad0de9db851517d3
haqqisadieda <haqqisadiedapandega@gmial.com>
2023-11-26 06:51:58 +0700

    Adding data logger body-parser cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import express, { type Application, type Request, type Response, type NextFunction } from 'express'
+import express, { type Application } from 'express'
+import { routes } from './routes'
+import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const port: number = 4000
 
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: '200', data: 'Hello World' })
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// cors
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
 })
 
+routes(app)
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+  logger.info(`Server is listening on port ${port}`)
 })
