26107b5de696f13f2048aeaacf670b792fc5ebf7
haqqisadieda <haqqisadiedapandega@gmial.com>
2023-11-26 20:41:57 +0700

    Connecting to MongoDB

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,21 +1,8 @@
-import { type Request, type Response, type NextFunction, Router } from 'express'
-import { logger } from '../utils/logger'
-import { createProductValidation } from '../validation/product.validation'
+import { Router } from 'express'
+import { createProduct, getProducts } from '../controllers/product.controller'
 
 export const ProductRouter: Router = Router()
 
-ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  logger.info('Success get product data')
-  return res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'Sepatu Sport', price: 500000 }] })
-})
-
-ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = createProductValidation(req.body)
-  if (error) {
-    const errorMessage: string = error.details[0].message
-    logger.error(`ERR: Product - Create = ${errorMessage}`)
-    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
-  }
-  logger.info('Success add new product')
-  res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
-})
+ProductRouter.get('/', getProducts)
+ProductRouter.get('/:name', getProducts)
+ProductRouter.post('/', createProduct)
