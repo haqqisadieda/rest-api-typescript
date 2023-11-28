6b9626df01f12ede94b3f20f364d1fc2b82d666a
haqqisadieda <haqqisadiedapandega@gmial.com>
2023-11-28 18:03:14 +0700

    Adding types and create data to DB method

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,24 +1,27 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { createProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { getProductFromDB } from '../services/product.service'
+import { addProductToDB, getProductFromDB } from '../services/product.service'
+import { v4 as uuidv4 } from 'uuid'
+import type ProductType from '../types/product.type'
 
-interface ProductType {
-  product_id: string
-  name: string
-  price: number
-  size: string
-}
-
-export const createProduct = (req: Request, res: Response, next: NextFunction) => {
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
+  req.body.product_id = uuidv4()
   const { error, value } = createProductValidation(req.body)
   if (error) {
     const errorMessage: string = error.details[0].message
     logger.error(`ERR: Product - Create = ${errorMessage}`)
-    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message })
   }
-  logger.info('Success add new product')
-  res.status(200).send({ status: true, statusCode: 200, message: 'Add product success', data: value })
+  try {
+    await addProductToDB(value)
+    logger.info('Success add new product')
+    res.status(200).send({ status: true, statusCode: 200, message: 'Add product success' })
+  } catch (error) {
+    const errorMessage: any = error
+    logger.error(`ERR: Product - Create = ${errorMessage}`)
+    return res.status(422).send({ status: false, statusCode: 422, message: error })
+  }
 }
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
